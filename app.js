ce70129e5c4e0ed96e63eefea327a358e42fb1b9
Oshamaru <callMeSam808@gmail.com>
2022-06-02 22:07:56 -1000

    Create index.html, app.js, gitignore; init npm; install express.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.listen(port, () => {
+  console.log(`Todolist-v1 is listening on port ${port}`);
+});
