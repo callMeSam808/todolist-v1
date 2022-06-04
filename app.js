3309ad13dae48b4884cfc5a0b66b8d66156f82d3
Oshamaru <callMeSam808@gmail.com>
2022-06-04 12:17:09 -1000

    Create form, input, and post req to add items to to-do list.

--- app.js
+++ app.js
@@ -11,39 +11,60 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
 
+let items = ["Buy food", "Cook food", "Eat Food"];
+
+
 app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = "";
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  let day = today.toLocaleDateString("en-US", options);
 
-  switch(currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-  }
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items
+  });
+});
 
-  res.render("list", {kindOfDay: day});
+
+app.post("/", (req, res) => {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 
 app.listen(port, () => {
   console.log(`Todolist-v1 is listening on port ${port}`);
 });
+
+
+// let currentDay = today.getDay();
+// let day = "";
+
+// switch(currentDay) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+//     break;
+// }
