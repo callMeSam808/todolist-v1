7bafcb9edf09f9790d2a0e1a952bdac190f29722
Oshamaru <callMeSam808@gmail.com>
2022-06-03 21:32:49 -1000

    Install ejs; add views directory and list.ejs; display current day of week.

--- app.js
+++ app.js
@@ -5,11 +5,42 @@ const app = express();
 const port = 3000;
 
 
+app.set('view engine', 'ejs');
+
+
 app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  let today = new Date();
+  let currentDay = today.getDay();
+  let day = "";
+
+  switch(currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+  }
+
+  res.render("list", {kindOfDay: day});
 });
 
 
